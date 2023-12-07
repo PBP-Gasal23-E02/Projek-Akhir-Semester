fd945115302bcd46dfd60846f3ee4c20aa2abf62
Aaron <aaron.kwek@ui.ac.id>
2023-12-07 19:01:15 +0700

    menambahkan koleksi buku dan filter

--- lib/apps/YourBook/screens/PinjamBukulist_form.dart
+++ lib/apps/YourBook/screens/PinjamBukulist_form.dart
@@ -4,184 +4,222 @@ import 'package:provider/provider.dart';
 import 'package:gourmet_labs/apps/YourBook/screens/menu.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 
-
 class InventoryFormPage extends StatefulWidget {
-    const InventoryFormPage({super.key});
+  const InventoryFormPage({super.key});
 
-    @override
-    State<InventoryFormPage> createState() => _InventoryFormPageState();
+  @override
+  State<InventoryFormPage> createState() => _InventoryFormPageState();
 }
 
 class _InventoryFormPageState extends State<InventoryFormPage> {
-    final _formKey = GlobalKey<FormState>();
-    String _judul = "";
-    int _durasiPinjam = 0;
-    String _catatan = "";
-    String _petugas = "";
+  final _formKey = GlobalKey<FormState>();
+  String _judul = "";
+  int _durasiPinjam = 0;
+  String _catatan = "";
+  String _petugas = "";
 
-    @override
-    Widget build(BuildContext context) {
-      final request = context.watch<CookieRequest>();
-        return Scaffold(
-          appBar: AppBar(
-            title: const Center(
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Form Pinjam Buku',
+          ),
+        ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      // drawer: const LeftDrawer(),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Text(
-                'Form Pinjam Buku',
+                "Silahkan add buku sesuai judul, judul yg terlau panjang dan mengandung enter, karakter lain akibat buruknya dataset bisa diisi subjudulnya saja",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            backgroundColor: Colors.indigo,
-            foregroundColor: Colors.white,
-          ),
-          // drawer: const LeftDrawer(),
-          body: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Judul Buku",
-                        labelText: "Judul Buku",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                      ),
-                      onChanged: (String? value) {
-                        setState(() {
-                          _judul = value!;
-                        });
-                      },
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return "Judul tidak boleh kosong!";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Petugas",
-                        labelText: "Petugas",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                      ),
-                      onChanged: (String? value) {
-                        setState(() {
-                          _petugas = value!;
-                        });
-                      },
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return "Petugas tidak boleh kosong!";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "durasi pinjam",
-                        labelText: "durasi pinjam",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                      ),
-                      onChanged: (String? value) {
-                        setState(() {
-                          _durasiPinjam = int.parse(value!);
-                        });
-                      },
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return "durasi pinjam tidak boleh kosong!";
-                        }
-                        if (int.tryParse(value) == null) {
-                          return "durasi pinjam harus berupa angka!";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Catatan",
-                        labelText: "Catatan",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        ),
-                      ),
-                      onChanged: (String? value) {
-                        setState(() {
-                          _catatan = value!;
-                        });
-                      },
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return "Catatan tidak boleh kosong!";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.indigo),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                              final response = await request.postJson(
-                              "http://127.0.0.1:8000/YourBook/create-flutter/",
-                              jsonEncode(<String, String>{
-                                  'judul': _judul,
-                                  'petugas' : _petugas,
-                                  'durasi': _durasiPinjam.toString(),
-                                  'catatan': _catatan,
-                              }));
-                              if (response['status'] == 'success') {
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(const SnackBar(
-                                  content: Text("Buku berhasil dipinjam!"),
-                                  ));
-                                  Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(builder: (context) => YourBookPage()),
-                                  );
-                              } else {
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(const SnackBar(
-                                      content:
-                                          Text("Terdapat kesalahan, silakan coba lagi."),
-                                  ));
-                              }
-                          }
-                        },
-                        child: const Text(
-                          "Save",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "aplikasi ini akan mengambil judul pertama berdasarkan dataset di main page yang mengandung subjudul",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
-        );
-    }
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Judul Buku",
+                  labelText: "Judul Buku",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _judul = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Judul tidak boleh kosong!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Petugas",
+                  labelText: "Petugas",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _petugas = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Petugas tidak boleh kosong!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "durasi pinjam",
+                  labelText: "durasi pinjam",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _durasiPinjam = int.parse(value!);
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "durasi pinjam tidak boleh kosong!";
+                  }
+                  if (int.tryParse(value) == null) {
+                    return "durasi pinjam harus berupa angka!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Catatan",
+                  labelText: "Catatan",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                onChanged: (String? value) {
+                  setState(() {
+                    _catatan = value!;
+                  });
+                },
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Catatan tidak boleh kosong!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Isilah dengan judul yang sesuai dengan dataset katalog buku pada halaman daftar buku",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "Buku dengan judul yang tidak valid tidak bisa dipinjam",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.indigo),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      final response = await request.postJson(
+                          "http://127.0.0.1:8000/YourBook/create-flutter/",
+                          jsonEncode(<String, String>{
+                            'judul': _judul,
+                            'petugas': _petugas,
+                            'durasi': _durasiPinjam.toString(),
+                            'catatan': _catatan,
+                          }));
+                      if (response['status'] == 'success') {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Buku berhasil dipinjam!"),
+                        ));
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => YourBookPage()),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content:
+                              Text("Terdapat kesalahan, silakan coba lagi."),
+                        ));
+                      }
+                    }
+                  },
+                  child: const Text(
+                    "Save",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
 }
