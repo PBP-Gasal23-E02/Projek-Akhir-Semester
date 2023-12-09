839eb4de15124f1987adbbf9d61198bcf56f7dc9
Aaron <aaron.kwek@ui.ac.id>
2023-12-09 10:31:09 +0700

    add fitur kembalikan buku

--- lib/apps/YourBook/screens/PinjamBukulist_form.dart
+++ lib/apps/YourBook/screens/PinjamBukulist_form.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:gourmet_labs/apps/YourBook/screens/menu.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
-
+import 'package:http/http.dart' as http;
+import 'package:gourmet_labs/apps/YourBook/models/Buku.dart';
 class InventoryFormPage extends StatefulWidget {
   const InventoryFormPage({super.key});
 
@@ -17,6 +18,40 @@ class _InventoryFormPageState extends State<InventoryFormPage> {
   int _durasiPinjam = 0;
   String _catatan = "";
   String _petugas = "";
+  String? selectedValue;
+  late Future<List<String>> listProduct;
+
+  @override
+  void initState() {
+    super.initState();
+    // Memanggil fungsi fetchProduct di initState
+    listProduct = fetchProduct();
+  }
+
+  Future<List<String>> fetchProduct() async {
+    var url = Uri.parse('https://gourmetlabs-e02-tk.pbp.cs.ui.ac.id/api/books/');
+    var response = await http.get(
+      url,
+      headers: {"Content-Type": "application/json"},
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object Product
+    List<String> productList = [];
+    for (var d in data) {
+      if (d != null) {
+        Buku buku = Buku.fromJson(d);
+        productList.add(buku.fields.title);
+      }
+    }
+
+    // Pastikan nilai awal selectedValue adalah unik
+    selectedValue = productList.isNotEmpty ? productList[0] : null;
+
+    return productList;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,48 +74,37 @@ class _InventoryFormPageState extends State<InventoryFormPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Silahkan add buku sesuai judul, judul yg terlau panjang dan mengandung enter, karakter lain akibat buruknya dataset bisa diisi subjudulnya saja",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
+                padding: const EdgeInsets.all(8.0),
+                child: FutureBuilder<List<String>>(
+                  future: listProduct,
+                  builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return DropdownButton<String>(
+                        value: selectedValue,
+                        onChanged: (String? value) {
+                          // This is called when the user selects an item.
+                          setState(() {
+                            selectedValue = value!;
+                            _judul = value;
+                          });
+                        },
+                        isExpanded: true,
+                        items: snapshot.data!
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      );
+                    }
+                  },
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "aplikasi ini akan mengambil judul pertama berdasarkan dataset di main page yang mengandung subjudul",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Judul Buku",
-                  labelText: "Judul Buku",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                ),
-                onChanged: (String? value) {
-                  setState(() {
-                    _judul = value!;
-                  });
-                },
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return "Judul tidak boleh kosong!";
-                  }
-                  return null;
-                },
-              ),
-            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextFormField(
@@ -157,16 +181,6 @@ class _InventoryFormPageState extends State<InventoryFormPage> {
               padding: const EdgeInsets.all(16.0),
               child: Text(
                 "Isilah dengan judul yang sesuai dengan dataset katalog buku pada halaman daftar buku",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Buku dengan judul yang tidak valid tidak bisa dipinjam",
                 style: TextStyle(
                   fontSize: 16.0,
                   fontWeight: FontWeight.bold,
